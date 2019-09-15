ff3f06589480754d5c60968c9319324bfe769409
Giorgio Garasto <giorgio@garasto.it>
2019-09-15 08:46:04 +0200

    refactor: Rewrite layout into a functional TSX component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import { Link } from 'gatsby';
 
 import { rhythm, scale } from '../utils/typography';
+import { PageProps } from '../utils/types';
 
-class Layout extends React.Component {
-  render() {
-    const { location, title, children } = this.props;
-    const rootPath = `${__PATH_PREFIX__}/`;
-    let header;
+declare const __PATH_PREFIX__: string;
 
-    if (location.pathname === rootPath) {
-      header = (
+const Layout: FunctionComponent<PageProps> = ({
+  location: { pathname },
+  title,
+  children,
+}) => (
+  <div
+    style={{
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      maxWidth: rhythm(24),
+      padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+    }}
+  >
+    <header>
+      {pathname === `${__PATH_PREFIX__}/` ? (
         <h1
           style={{
             ...scale(1.5),
@@ -29,9 +39,7 @@ class Layout extends React.Component {
             {title}
           </Link>
         </h1>
-      );
-    } else {
-      header = (
+      ) : (
         <h3
           style={{
             fontFamily: 'Montserrat, sans-serif',
@@ -49,26 +57,14 @@ class Layout extends React.Component {
             {title}
           </Link>
         </h3>
-      );
-    }
-    return (
-      <div
-        style={{
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with{' '}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    );
-  }
-}
+      )}
+    </header>
+    <main>{children}</main>
+    <footer>
+      © {new Date().getFullYear()}, Built with{' '}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </footer>
+  </div>
+);
 
 export default Layout;
