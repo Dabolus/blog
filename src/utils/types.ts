2473bc7d3fdd6d025078bf7ccdbd5891701a0415
Giorgio Garasto <giorgio@garasto.it>
2019-09-15 09:00:31 +0200

    feat: Make page data of type never by default

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,8 +2,9 @@ import { SitePageContext } from '../generated/graphql-types';
 
 export * from '../generated/graphql-types';
 
-export interface PageProps<T> {
+export interface PageProps<T = never> {
   data: T;
   pageContext: SitePageContext;
   location: Location;
+  title: string;
 }
