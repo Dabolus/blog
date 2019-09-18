e86c295c507495026eef68a2d08a85868904d84b
Giorgio Garasto <giorgio@garasto.it>
2019-09-18 08:06:19 +0200

    feat: Start implementing Firestore to MD transformer

---
+++ plugins/gatsby-transformer-firestore-to-post/gatsby-node.js
@@ -0,0 +1,3 @@
+exports.createSchemaCustomization = require('./create-schema-customization');
+exports.onCreateNode = require('./on-create-node');
+exports.setFieldsOnGraphQLNodeType = require('./set-fields-on-graphql-node-type');
---
+++ plugins/gatsby-transformer-firestore-to-post/on-create-node.js
@@ -0,0 +1,72 @@
+const grayMatter = require('gray-matter');
+const _ = require('lodash');
+
+module.exports = async (
+  {
+    node,
+    actions: { createNode, createParentChildLink },
+    createNodeId,
+    reporter,
+    createContentDigest,
+  },
+  pluginOptions,
+) => {
+  // We only care about posts.
+  if (node.internal.type !== 'Post') {
+    return {};
+  }
+
+  const content = node;
+
+  try {
+    let data = grayMatter(content, pluginOptions);
+
+    if (data.data) {
+      data.data = _.mapValues(data.data, value => {
+        if (_.isDate(value)) {
+          return value.toJSON();
+        }
+        return value;
+      });
+    }
+
+    let markdownNode = {
+      id: createNodeId(`${node.id} >>> MarkdownRemark`),
+      children: [],
+      parent: node.id,
+      internal: {
+        content: data.content,
+        type: `MarkdownRemark`,
+      },
+    };
+
+    markdownNode.frontmatter = {
+      title: ``, // always include a title
+      ...data.data,
+    };
+
+    markdownNode.excerpt = data.excerpt;
+    markdownNode.rawMarkdownBody = data.content;
+
+    // Add path to the markdown file path
+    if (node.internal.type === `File`) {
+      markdownNode.fileAbsolutePath = node.absolutePath;
+    }
+
+    markdownNode.internal.contentDigest = createContentDigest(markdownNode);
+
+    createNode(markdownNode);
+    createParentChildLink({ parent: node, child: markdownNode });
+
+    return markdownNode;
+  } catch (err) {
+    reporter.panicOnBuild(
+      `Error processing Markdown ${
+        node.absolutePath ? `file ${node.absolutePath}` : `in node ${node.id}`
+      }:\n
+      ${err.message}`,
+    );
+
+    return {}; // eslint
+  }
+};
