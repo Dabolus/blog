519b481ace2de9668664493b719bbd726dd6c437
Giorgio Garasto <giorgio@garasto.it>
2019-09-20 07:45:55 +0200

    feat: Experiments with cloning and building from a function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,28 @@
 import * as functions from 'firebase-functions';
+import { resolve } from 'path';
+import { exec as execCb } from 'child_process';
+import { tmpdir } from 'os';
+import git from 'nodegit';
+
+const tmp = tmpdir();
+
+const exec = (command: string) =>
+  new Promise((resolve, reject) =>
+    execCb(command, (err, stdout) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(stdout);
+    }),
+  );
 
 export const updateBlog = functions.firestore
   .document('posts/{slug}')
-  .onWrite((change, context) => {
-    // Do the magic
+  .onWrite(async () => {
+    await git.Clone.clone(
+      'https://github.com/Dabolus/blog.git',
+      resolve(tmp, 'blog'),
+    );
+    process.chdir(resolve(tmp, 'blog'));
+    await exec('yarn build');
   });
