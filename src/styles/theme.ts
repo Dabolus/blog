35bf3a21f5ccd760e6acbac887cfa88d2dfde0e7
Giorgio Garasto <giorgio@garasto.it>
2019-12-07 12:25:08 +0100

    fix: Correctly configure theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,24 +2,13 @@ import { DefaultTheme } from './default-theme';
 
 const theme: DefaultTheme = {
   layout: {
-    dark: {
-      primaryColor: '#fff',
-      secondaryColor: 'rgba(255, 255, 255, 0.7)',
-      accent: '#ff6659',
-      defaultBackground: '#212121',
-      contentBackground: '#303030',
-      cardBackground: '#424242',
-      separator: 'rgba(255, 255, 255, 0.12)',
-    },
-    light: {
-      primaryColor: 'rgba(0, 0, 0, 0.87)',
-      secondaryColor: 'rgba(0, 0, 0, 0.54)',
-      accent: '#d32f2f',
-      defaultBackground: '#f0f0f0',
-      contentBackground: '#fafafa',
-      cardBackground: '#fff',
-      separator: 'rgba(0, 0, 0, 0.12)',
-    },
+    primaryColor: 'var(--theme-primary-color)',
+    secondaryColor: 'var(--theme-secondary-color)',
+    accent: 'var(--theme-accent)',
+    defaultBackground: 'var(--theme-default-background)',
+    contentBackground: 'var(--theme-content-background)',
+    cardBackground: 'var(--theme-card-background)',
+    separator: 'var(--theme-separator)',
   },
   breakpoints: {
     xs: `425px`,
